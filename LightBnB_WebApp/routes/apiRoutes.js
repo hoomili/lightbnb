9f217e8b457757194174409c5dcb211ca2cae9b2
hoomili <hasadian@hotmail.com>
2023-05-04 20:18:18 -0700

    search tab will give correct result

--- LightBnB_WebApp/routes/apiRoutes.js
+++ LightBnB_WebApp/routes/apiRoutes.js
@@ -4,6 +4,7 @@ const database = require("../db/database");
 const router = express.Router();
 
 router.get("/properties", (req, res) => {
+  console.log('me', res.query);
   database
     .getAllProperties(req.query, 20)
     .then((properties) => res.send({ properties }))
